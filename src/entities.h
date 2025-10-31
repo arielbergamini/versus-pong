b9de8b972e1fe3815ae132841566f33c21237a94
arielbergamini <arielbergamini263@gmail.com>
2025-10-31 17:34:13 -0500

    Collision physics, input handling for local client

--- src/entities.h
+++ src/entities.h
@@ -6,6 +6,20 @@ const int BALL_HEIGHT = 15;
 const int PADDLE_WIDTH = 10;
 const int PADDLE_HEIGHT = 80;
 
+const int BALL_SPEED = 250;
+
+enum class CollisionType {
+    None,
+    Top,
+    Middle,
+    Bottom
+};
+
+struct Contact {
+    CollisionType type;
+    float penetration;
+};
+
 class Vec2d {
 
     public:
@@ -33,11 +47,35 @@ class Vec2d {
 //TODO --> ball physics!
 class Ball {
     public:
-        Ball(Vec2d position) : position(position) {
+        Ball(Vec2d position, Vec2d velocity) 
+        : position(position), velocity(velocity) {
             rect.x = static_cast<int>(position.x);
             rect.y = static_cast<int>(position.y);
             rect.w = BALL_WIDTH;
             rect.h = BALL_HEIGHT;
+        }
+
+        void PaddleCollision(Contact const &contact) {
+            position.x += contact.penetration;
+            velocity.x = -velocity.x;
+
+            if (contact.type == CollisionType::Top) {
+                velocity.y = -.75f * BALL_SPEED;
+            } else if (contact.type == CollisionType::Bottom) {
+                velocity.y = 0.75f * BALL_SPEED;
+            }
+        }
+
+        void Update(float dt) {
+            position += velocity * dt;
+
+            if (position.y <= 0) {
+                position.y = 0;
+                velocity.y *= -1;
+            } else if (position.y + BALL_HEIGHT >= 480.0f) {
+                position.y = 480.0f - BALL_HEIGHT;
+                velocity.y *= -1;
+            }
         }
 
         void Draw(SDL_Renderer* renderer) {
@@ -47,7 +85,7 @@ class Ball {
             SDL_RenderFillRect(renderer, &rect);
         }
 
-        Vec2d position;
+        Vec2d position, velocity;
         SDL_Rect rect{};
 };
 
@@ -55,18 +93,95 @@ class Ball {
 //TODO --> input sync & paddle physics!
 class Paddle {
     public:
-        Paddle(Vec2d position) : position(position) {
+        Vec2d position, velocity;
+        SDL_Rect rect{};
+
+        Paddle(Vec2d position, Vec2d velocity)
+         : position(position), velocity(velocity) {
             rect.x = static_cast<int>(position.x);
             rect.y = static_cast<int>(position.y);
             rect.w = PADDLE_WIDTH;
             rect.h = PADDLE_HEIGHT;
         }
 
-        Vec2d position;
-        SDL_Rect rect{};
+        void Update(float moveDir, float dt) {
+
+            if(moveDir == 0.0f) {
+                velocity.y *= 0.9f;
+            } else {
+                velocity.y = moveDir * 300.0f;
+            }
+
+            position += velocity * dt;
+            
+            //screen bounds handling
+            if (position.y < 0) {
+                position.y = 0; //position restricted to top of screen
+            } else if (position.y > (480.0 - PADDLE_HEIGHT )) {
+                position.y = 480.0 - PADDLE_HEIGHT; //restricted to bottom of screen
+            }
+        }
 
         void Draw(SDL_Renderer* renderer) {
+            rect.x = static_cast<int>(position.x);
             rect.y = static_cast<int>(position.y);
+            rect.w = PADDLE_WIDTH;
+            rect.h = PADDLE_HEIGHT;
+
             SDL_RenderFillRect(renderer, &rect);
         }
+};
+
+
+Contact Collision(Ball const &ball, Paddle const &paddle) {
+    float ballLeft = ball.position.x;
+    float ballRight = ball.position.x + BALL_WIDTH;
+    float ballTop = ball.position.y;
+    float ballBottom = ball.position.y + BALL_HEIGHT;
+
+    float paddleLeft = paddle.position.x;
+    float paddleRight = paddle.position.x + PADDLE_WIDTH;
+    float paddleTop = paddle.position.y;
+    float paddleBottom = paddle.position.y + PADDLE_HEIGHT;
+
+    Contact contact{};
+    contact.type = CollisionType::None;
+    contact.penetration = 0.0f;
+
+    if (ballLeft >= paddleRight) {
+        return contact;
+    }
+    if (ballRight <= paddleLeft) {
+        return contact;
+    }
+    if (ballTop >= paddleBottom) {
+        return contact;
+    }
+    if (ballBottom <= paddleTop) {
+        return contact;
+    }
+    
+    float paddleRangeUpper = paddleBottom - (2.0f * PADDLE_HEIGHT / 3.0f);
+    float paddleRangeMiddle = paddleBottom - (PADDLE_HEIGHT / 3.0F);
+
+    //left paddle
+    if (ball.velocity.x < 0) {
+        contact.penetration = paddleRight - ballLeft;
+    }
+    //right paddle
+    else if (ball.velocity.x > 0) {
+        contact.penetration = paddleLeft - ballRight;
+    }
+
+    if ((ballBottom > paddleTop) && (ballBottom < paddleRangeUpper)) {
+        contact.type = CollisionType::Top;
+    }
+    else if ((ballBottom > paddleRangeUpper) && (ballBottom < paddleRangeMiddle)) {
+        contact.type = CollisionType::Middle;
+    }
+    else {
+        contact.type = CollisionType::Bottom;
+    }
+    return contact;
+
 };
--- src/game.cpp
+++ src/game.cpp
@@ -1,57 +1,103 @@
 #include "entities.h"
-
+#include <chrono>
 SDL_Window* window = nullptr;
 SDL_Renderer* renderer = nullptr;
 
+
+//TODO --> read local input and move paddle1
 bool loop () {
-    static const unsigned char* keys = SDL_GetKeyboardState(NULL);
     SDL_Event event;
     SDL_Rect rect;
-    static int mx0, my0, mx1, my1 = -1;
+    bool keep_window_open = true;
+    float dt = 0.016f; //FIXME --> placeholder dt for testing
+    const int BALL_SPEED = 250;
 
+    //----------Create game objects once
     SDL_SetRenderDrawColor(renderer, 0, 0, 0, 0);
     SDL_RenderClear(renderer);
-    SDL_RenderFillRect(renderer, &rect);
 
-    bool keep_window_open = true;
+    Ball ball(
+        Vec2d(680.0 / 2.0f,  480.0 / 2.0f),
+        Vec2d(BALL_SPEED, 0.0f));
 
+    Paddle paddle1(
+        Vec2d(50.0f, 480.0 / 2.0f),
+        Vec2d(0.0f, 0.0f));
+
+    Paddle paddle2(
+        Vec2d(680.0 - 50.0f, 480.0 / 2.0f),
+        Vec2d(0.0f, 0.0f));   
+
+    //GAME LOOP-------------------------------------------
+    auto lastTime = std::chrono::high_resolution_clock::now();
 
     while (keep_window_open) {
-        //event handling logic
+        //frame dt
+
+        auto currentTime = std::chrono::high_resolution_clock::now();
+        std::chrono::duration<float> elapsed = currentTime - lastTime;
+        dt = elapsed.count();
+        lastTime = currentTime;
+
+        if (dt > 0.05f) dt = 0.05f; //damping
+
+        //handling window events
         while (SDL_PollEvent(&event)) { 
-                switch(event.type) 
-                {
-                    case SDL_QUIT: //close window if user wants to quit
+                if (event.type == SDL_QUIT) { //close window if user wants to quit
                         keep_window_open = false;
-                        break;
                 }
+            }
+
+            //handle LOCAL CLIENT (paddle1)
+            //
+            //
+            const Uint8* keys = SDL_GetKeyboardState(nullptr);
+            float moveDir = 0.0f;
+            //player 1
+            if (keys[SDL_SCANCODE_W]) moveDir -= 1.0f;
+            if (keys[SDL_SCANCODE_S]) moveDir += 1.0f;
+
+            //update object positions
+            paddle1.Update(moveDir, dt);
+            ball.Update(dt);
+
+            //check collisions
+            Contact contact1 = Collision(ball, paddle1);
+            if (contact1.type != CollisionType::None) {
+                ball.PaddleCollision(contact1);
+            }
+
+            Contact contact2 = Collision(ball, paddle2);
+            if (contact2.type != CollisionType::None) {
+                ball.PaddleCollision(contact2);
+            }
+            //===TODO --> SEND local input to server
+            //
+            //
+            //auto stopTime = std::chrono::high_resolution_clock::now();
+            //dt = std::chrono::duration<float, std::chrono::milliseconds::period>(stopTime - startTime).count();
+
+            //------Rendering-------
+            //clear bg to black
+            SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
+            SDL_RenderClear(renderer);
+
+            //paddle colors set to pink
+            SDL_SetRenderDrawColor(renderer, 233, 4, 122, 0.8);
+            
+            //draw objects to window
+            paddle1.Draw(renderer);
+            paddle2.Draw(renderer);
+
+            ball.Draw(renderer);
+
+            SDL_RenderPresent(renderer);
         }
-
-        //----------Create game objects & draw to window-------------------
-        SDL_SetRenderDrawColor(renderer, 233, 4, 122, 0.8);
-
-        Ball ball(
-            Vec2d((680.0 / 2.0f) - (BALL_WIDTH / 2.0f),
-            (480.0 / 2.0f) - (BALL_WIDTH / 2.0f)));
-
-        Paddle paddle1(
-            Vec2d(50.0f, (480.0 / 2.0f) - (PADDLE_HEIGHT / 2.0f)));
-        Paddle paddle2(
-            Vec2d(680.0 - 50.0f, (480.0 / 2.0f) - (PADDLE_HEIGHT / 2.0f)));    
-
-        //draw objects to window
-        ball.Draw(renderer);
-        paddle1.Draw(renderer);
-        paddle2.Draw(renderer);
-        SDL_RenderPresent(renderer);
-    }
 
     SDL_DestroyWindow(window);
     SDL_Quit();
-
     return false;
 }
-
 
 //initializing sdl
 bool init () {
